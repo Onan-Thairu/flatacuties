46e09f3987f2eba52e11bd88147dc561a5dfed9a
OnanThairu <thairuonan@gmail.com>
2022-06-20 16:38:37 +0300

    displayed character details

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-// Your code here
+const baseUrl = 'http://localhost:3000'
+
+
+
+fetch( `${baseUrl}/characters`)
+.then(response =>response.json())
+.then(data => {
+    let characterBar = document.querySelector('div#character-bar')
+    data.forEach(char => {
+        let spanTag = document.createElement('span')
+        spanTag.textContent = char.name
+        spanTag.addEventListener('click', () => {
+            const name = document.querySelector('#name')
+            name.textContent = char.name
+            const img = document.querySelector('#image')
+            img.src = char.image
+        })
+        characterBar.appendChild(spanTag)
+    })
+})
+
+// function displayInfo(character) {
+//     fetch(`${baseUrl}/characters/${character.id}`)
+//       .then(response => response.json())
+//       .then(data => console.log(data.name))
+// }
