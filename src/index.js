86397d42f800ebf6e6c003ca1235a9db21fea142
gloria oduor <gloriaoduor@gmail.com>
2022-06-21 10:05:17 +0300

    edited the functionalities added in previous commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 const baseUrl = 'http://localhost:3000'
 
 
@@ -18,4 +20,18 @@ fetch( `${baseUrl}/characters`)
         characterBar.appendChild(spanTag)
     })
 })
+
+    //grab the voting form by ID 
+    let votesForm = document.getElementById("votes-form")
+
+    votesForm.addEventListener('submit', (e) =>{
+        e.preventDefault(); //prevents the page from reloading when submit is clicked
+        addVote(e.target.votes.value) //the value in the input box is passed as an argument to the function
+        votesForm.reset() //input box becomes blank on submit click
+    })
+   
+
+function addVote(voteCount){
+    document.getElementById('vote-count').innerHTML = `${voteCount}`
+}
 
